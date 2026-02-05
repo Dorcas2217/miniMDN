78087e878f12a63f82642765dbb8825bce07534a
Dorcas <dorcaskueze@gmail.com>
2026-02-05 09:38:08 +0100

    add models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,26 @@
+import uuid
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Fleet(models.Model):
+    name = models.CharField(max_length=100) 
+    # one fleet belongs to one user
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='fleets')
+
+    class Meta:
+        # contraint to ensure unique fleet names per user
+        constraints = [
+            models.UniqueConstraint(fields=['name', 'owner'], name='unique_fleet_per_user')
+        ]
+
+    def __str__(self):
+        return f"{self.name} (Propriétaire: {self.owner.username})"
+
+class Device(models.Model):
+    serial_number = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    fleet = models.ForeignKey(Fleet, on_delete=models.CASCADE, related_name='devices')
+    # ex os version
+    os_version = models.IntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return f"Device {self.serial_number}"
---
+++ core/serializer.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+from .models import Fleet, Device
+
+# Sérializer for Fleets
+class FleetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Fleet
+        fields = ['id', 'name', 'owner']
+        read_only_fields = ['owner'] # owner défini automatiquement.
+
+# Serializer for Users
+class UserSerializer(serializers.ModelSerializer):
+    fleets = FleetSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'date_joined', 'fleets']
+
+# Serializer for Devices
+class DeviceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Device
+        fields = ['id', 'serial_number', 'fleet', 'os_version']
+        
